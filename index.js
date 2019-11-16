abad0a1c6b37552b8ad44505b06bb88480e5303e
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-11-16 13:36:43 -0600

    Get the general layout of the page done. Need to start using components to render colors.

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 // Write your app here! (HINT: First thing should be a call to ReactDOM.render()... )
 
+function RandomButton () {
+  return <div className="text-center bg-dark fixed-top">
+  <button className="btn btn-secondary m-1">RANDOMIZE COLORS</button> </div>
+}
+
+class Color extends React.Component {
+  state = {
+    color: 'blue',
+    hex: '#FF00FF'
+  }
+
+  render() {
+    return(
+      <div className="w-100 d-flex" style={{height: "100vh"}}>
+        <button style={{height: "10vh"}}>LOCK</button>
+      </div>
+    )
+  }
+}
+
 const App = function() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <RandomButton />
+      <div className="d-flex flex-row">
+      <Color />
+      <Color />
+      <Color />
+      <Color />
+      <Color />
+      </div>
     </div>)
 }
 
