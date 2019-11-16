79ea3c7b00e7989766074676a38b6277439fcd2e
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-11-16 12:50:44 -0600

    Initial commit. Make sure that the App component works.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your app here! (HINT: First thing should be a call to ReactDOM.render()... )
+
+const App = function() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
